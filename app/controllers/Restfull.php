e1d7aabaeda558f0fc8ef7d3f6cac0ce6ed7a5cc
Andreas Kollaros <andreas.kollaros@gmail.com>
2012-07-09 16:12:52 +0300

    add docblocks. add edit, update and delete action. add minor improvements

--- app/controllers/Restfull.php
+++ app/controllers/Restfull.php
@@ -5,6 +5,15 @@
 /**
  * The restfull controller.
  *
+ * Restfull controller helps to easy create crud actions for a model.
+ * Assuming that the Controller has the same name with your model. If you have 
+ * a PostsController then Restfull controller will assume that your resource is 
+ * the PostModel. 
+ *
+ * Notice that controller name is defined as `Posts`, plural, and model is 
+ * defind as `Post` singular. RestfullController uses Inflect class to 
+ * determinate the name of the Model to load {@link _get_model_name()}
+ *
  */
 abstract class RestfullController extends ApplicationController
 {
@@ -20,12 +29,19 @@ abstract class RestfullController extends ApplicationController
     public function indexAction()
     {
         $collection = $this->getResourceCollection();
-        $this->getView()->assign('collection',$collection);
+        $this->getView()->assign('collection', $collection);
     }
 
-    public function showAction()
+    /**
+     * Loads and displays a single resource asccording to Request::getParams() 
+     * values. 
+     *
+     * By default the uri to load a resource is /module/controller/show/id/1 
+     * showAction uses
+     */
+    public function showAction($id)
     {
-        $resource = $this->getResource();
+        $resource = $this->getResource($id);
         
         if (null === $resource) {
             return $this->forwardTo404();
@@ -44,15 +60,11 @@ abstract class RestfullController extends ApplicationController
     public function createAction()
     {
         $model = $this->_get_model_name();
-        $param_name = Inflect::underscore(str_replace('Model','',$model));
-        $post = $this->getRequest()->getPost();
-        $resource = new $model($post[$param_name]);
+
+        $resource = new $model($this->_get_resource_params($model));
 
         if ($resource->save()) {
-            $url = "/" 
-                . Inflect::underscore($this->getRequest()->getControllerName()) 
-                . "/index";
-            $this->redirect($url);
+            $this->redirect($this->_get_index_url());
         } else {
             $this->render('new', array('resource'=>$resource));
         }
@@ -61,18 +73,55 @@ abstract class RestfullController extends ApplicationController
     public function editAction()
     {
 
+        $resource = $this->getResource();
+        
+        if (null === $resource) {
+            return $this->forwardTo404();
+        } else {
+            $this->getView()->assign('resource',$resource);
+        }
     }
 
     public function updateAction()
     {
+        $resource = $this->getResource();
+        
+        if (null === $resource) {
+            return $this->forwardTo404();
+        } 
+        
+        $model = $this->_get_model_name();
 
+        if ($resource->updateAttributes($this->_get_resource_params($model))) {
+            $this->redirect($this->_get_index_url());
+        } else {
+            $this->render('edit', array('resource'=>$resource));
+        }
     }
 
     public function deleteAction()
     {
+        $resource = $this->getResource();
+        
+        if (null === $resource) {
+            return $this->forwardTo404();
+        }
 
+        $this->resource->destroy();
+
+        $this->redirect($this->_get_index_url());
     }
 
+    /**
+     * Creates a resource collection.
+     *
+     * By default it finds all elements of the resource. You can overload this 
+     * methods to return custom queries of the resource like pagination and/or 
+     * search.
+     *
+     * @return Orm\Mysql\Collection A collection object handling elements of 
+     *                              the resource
+     */
     public function getResourceCollection()
     {
         $model = $this->_get_model_name();
@@ -80,11 +129,11 @@ abstract class RestfullController extends ApplicationController
         return $model::find()->fetchAll();
     }
 
-    public function getResource()
+    public function getResource($id)
     {
         if (null === $this->_resource) {
             $model = $this->_get_model_name();
-            $this->_resource = $model::findById($this->getRequest()->getParam('id'))
+            $this->_resource = $model::findById($id)
                 ->fetch();
         }
         return $this->_resource;
@@ -94,5 +143,22 @@ abstract class RestfullController extends ApplicationController
     {
         return Inflect::singularize($this->getRequest()->getControllerName())
             . "Model";       
+    }
+
+    private function _get_resource_params($model)
+    {
+        $param_name = Inflect::underscore(str_replace('Model','',$model));
+        $post = $this->getRequest()->getPost();
+
+        return $post[$param_name];
+    }
+
+    protected function get_index_url()
+    {
+        $module = $this->getRequest()->getModuleName();
+        return "/"
+            . ('index' == strtolower($module) ? null : $module. "/")
+            . Inflect::underscore($this->getRequest()->getControllerName()) 
+            . "/index";       
     }
 }
