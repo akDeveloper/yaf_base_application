3fa4bffc2bfc0ff0d837f7cba4ab706235197d53
FightingMan <gianjason@gmail.com>
2013-03-26 22:04:49 +0800

    generate a random key every request

--- app/plugins/AuthToken.php
+++ app/plugins/AuthToken.php
@@ -2,7 +2,7 @@
 
 /* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
 /**
- * Sets an authenticity token to session and validates it against POST 
+ * Sets an authenticity token to session and validates it against POST
  * submissions.
  *
  * To enable it set it On at config/application.ini file
@@ -15,20 +15,20 @@
  * <input type="hidden" name="_auth_token" value="<?php echo Yaf\Session::getInstance()->auth_token ?>">
  * </code>
  *
- * After submission of the form, the plugin will attempt to validate the 
+ * After submission of the form, the plugin will attempt to validate the
  * auth_token an will throw an \Exception if tokens are not equal.
  */
 class AuthTokenPlugin extends Yaf\Plugin_Abstract
 {
 
-    public function routerShutdown(Yaf\Request_Abstract $request , 
+    public function routerShutdown(Yaf\Request_Abstract $request ,
         Yaf\Response_Abstract $response
     ) {
         $this->auth_token();
     }
 
-    public function dispatchLoopStartup(Yaf\Request_Abstract $request, 
-        Yaf\Response_Abstract $response 
+    public function dispatchLoopStartup(Yaf\Request_Abstract $request,
+        Yaf\Response_Abstract $response
     ){
 
         $this->verify_auth_token($request);
@@ -37,19 +37,21 @@ class AuthTokenPlugin extends Yaf\Plugin_Abstract
     protected function verify_auth_token($request)
     {
         $config = Yaf\Application::app()->getConfig();
-        
-        if (   $config['application']['protect_from_csrf']
-            && $request->isPost())
+
+        if ( $post = $request->getPost())
         {
-            $post = $request->getPost();
-            if (   !isset($post['_auth_token']) 
+            if (   !isset($post['_auth_token'])
                 || $post['_auth_token'] !== $this->auth_token()
             ){
                 throw new \Exception('Invalid authenticity token!');
+            } else {
+                $session = Yaf\Session::getInstance();
+                $session->auth_token = NULL;
+                $this->auth_token();
             }
         }
     }
-    
+
     /**
      * Creates a random token, ancodes it with Base64 and stores it to session
      *
